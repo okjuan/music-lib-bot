a8b2c4e0c9d9d214b7072a9cb3a50d15022697eb
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2021-03-10 23:20:05 -0800

    Get recently added albums

--- music.lib.bot.py
+++ music.lib.bot.py
@@ -1,8 +1,12 @@
+from datetime import datetime, timedelta
 import os
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+
+ISO8601_TIMESTAMP = "%Y-%m-%dT%H:%M:%SZ"
+NUM_DAYS_TO_LOOK_BACK = 1
 
 def get_spotify_creds():
     client_id = os.environ.get("SPOTIFY_CLIENT_ID")
@@ -14,12 +18,22 @@ def get_spotify_creds():
 def get_spotify_bearer_token():
     return os.environ.get("SPOTIFY_BEARER_TOKEN")
 
+def add_album_to_playlist(album):
+    print(f"Ok, this is where I do stuff with {album['name']} by {album['artists'][0]['name']}")
+
 scope = "user-library-read"
 client_id, client_secret = get_spotify_creds()
 
 auth = SpotifyOAuth(scope=scope)
 spotify = spotipy.Spotify(auth_manager=auth)
-results = spotify.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+# dict w/ keys: ['href', 'items', 'limit', 'next', 'offset', 'previous', 'total']
+results = spotify.current_user_saved_albums()
+
+# list, each item is a dict w/ keys: ['added_at', 'album']
+for album in results['items']:
+    time_added = datetime.strptime(album['added_at'], ISO8601_TIMESTAMP)
+    now = datetime.utcnow()
+    look_back = timedelta(NUM_DAYS_TO_LOOK_BACK)
+    if now - look_back < time_added:
+        add_album_to_playlist(album['album'])
