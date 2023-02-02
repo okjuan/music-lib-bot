okjuan/music-lib-bot

commit 8286326813521a2af0c5c25eba8a8a2d168382a0

Organize code into folders (#55)
A main benefit is most code doesn't know about Spotify-specific stuff now

src/packages/music_management/playlist_creator.py
@@ -3,8 +3,8 @@ from random import shuffle
 
 
 class PlaylistCreator:
-    def __init__(self, spotify_client, my_music_lib, music_util, info_logger):
-        self.spotify_client = spotify_client
+    def __init__(self, music_api_client, my_music_lib, music_util, info_logger):
+        self.music_api_client = music_api_client
         self.my_music_lib = my_music_lib
         self.music_util = music_util
         self.info_logger = info_logger
@@ -17,7 +17,7 @@ class PlaylistCreator:
         self.info_logger(f"Creating '{album_group['description']}' playlist from {len(album_group['albums'])} albums...")
         self.my_music_lib.create_playlist(
             album_group["description"],
-            [track.uri for track in tracks],
+            tracks,
             description="created by playlist_creator"
         )
         self.info_logger(f"Playlist created!")
@@ -50,7 +50,7 @@ class PlaylistCreator:
         self.info_logger(f"Creating '{playlist_title}' playlist...")
         self.my_music_lib.create_playlist(
             playlist_title,
-            [track.uri for track in tracks],
+            tracks,
             description="created by playlist_creator"
         )
 
@@ -61,7 +61,7 @@ class PlaylistCreator:
         tracks_by_album = defaultdict(list)
         playlist = get_playlist()
         for track in playlist.get_tracks():
-            tracks_by_album[track.album_id].append(track)
+            tracks_by_album[track].append(track)
 
         most_popular_tracks_per_album, num_tracks_per_album = [], get_num_tracks_per_album()
         for _, tracks in tracks_by_album.items():
@@ -72,9 +72,9 @@ class PlaylistCreator:
         new_playlist_name = get_new_playlist_name()
         self.info_logger(f"Created your new playlist '{new_playlist_name}' containing {len(most_popular_tracks_per_album)} tracks!")
 
-        track_uris = [track.id for track in most_popular_tracks_per_album]
-        shuffle(track_uris)
-        self.my_music_lib.create_playlist(new_playlist_name, track_uris)
+        shuffle(most_popular_tracks_per_album)
+        self.my_music_lib.create_playlist(
+            new_playlist_name, most_popular_tracks_per_album)
 
     def _get_discography(self, get_artist):
         artist = get_artist()

src/app/music_lib_bot.py
@@ -3,13 +3,13 @@
 import sys
 sys.path.extend(['.', '../'])
 
-from app.lib.music_util import MusicUtil
-from app.lib.my_music_lib import MyMusicLib
-from app.lib.playlist_creator import PlaylistCreator
-from app.lib.playlist_stats import PlaylistStats
-from app.lib.playlist_updater import PlaylistUpdater
+from packages.music_management.music_util import MusicUtil
+from packages.music_management.my_music_lib import MyMusicLib
+from packages.music_management.playlist_creator import PlaylistCreator
+from packages.music_management.playlist_stats import PlaylistStats
+from packages.music_management.playlist_updater import PlaylistUpdater
 from app.lib.interactive_option_picker import InteractiveOptionPicker
-from app.lib.spotify_client_wrapper import SpotifyClientWrapper
+from packages.music_api_clients.spotify import Spotify
 from app.lib.console_ui import ConsoleUI
 
 DEFAULT_LOOK_AT_ENTIRE_LIBRARY = False
@@ -26,13 +26,13 @@ MAX_NUM_TRACKS_TO_ADD = 100
 
 
 class MusicLibBot:
-    def __init__(self, spotify_client, my_music_lib, music_util, ui):
-        self.spotify_client = spotify_client
+    def __init__(self, music_api_client, my_music_lib, music_util, ui):
+        self.music_api_client = music_api_client
         self.my_music_lib = my_music_lib
         self.music_util = music_util
         self.ui = ui
         self.playlist_creator = PlaylistCreator(
-            self.spotify_client,
+            self.music_api_client,
             self.my_music_lib,
             self.music_util,
             self.ui.tell_user,
@@ -50,7 +50,7 @@ class MusicLibBot:
             playlist,
             self.my_music_lib,
             self.music_util,
-            self.spotify_client,
+            self.music_api_client,
             self.ui.tell_user,
             self.playlist_stats,
         )
@@ -69,9 +69,7 @@ class MusicLibBot:
         song_attribute_ranges = self.music_util.get_lenient_song_attribute_ranges(
             playlist)
         recommended_tracks_by_percentage = self.music_util.get_recommendations_based_on_tracks(
-            [track.id for track in playlist.get_tracks()],
-            song_attribute_ranges,
-        )
+            playlist.get_tracks(), song_attribute_ranges)
         if len(recommended_tracks_by_percentage) == 0:
             self.ui.tell_user("Sorry, couldn't find recommendations to add :(")
             return
@@ -89,7 +87,8 @@ class MusicLibBot:
 
         def recommended_track_pick_handler(recommended_tracks_with_percentage):
             self.my_music_lib.add_tracks_to_playlist(
-                playlist.id, [track.id for track in recommended_tracks_with_percentage["tracks"]])
+                playlist, recommended_tracks_with_percentage["tracks"])
+
         def get_recommended_track_description(recommended_tracks_with_percentage):
             percentage = int(recommended_tracks_with_percentage['recommended_percentage'] * 100)
             preamble = f"Recommended with {percentage}% confidence:\n- "
@@ -251,7 +250,7 @@ class MusicLibBot:
 
     def _get_artist_from_user(self):
         artist_name = self.ui.get_non_empty_string("What artist interests you?")
-        matching_artists = self.spotify_client.get_matching_artists(artist_name)
+        matching_artists = self.music_api_client.get_matching_artists(artist_name)
         if matching_artists == []:
             self.ui.tell_user(f"Sorry, I couldn't find an artist by the name '{artist_name}'")
             return None
@@ -302,11 +301,11 @@ class MusicLibBot:
 
 
 def main():
-    spotify_client_wrapper = SpotifyClientWrapper()
+    spotify = Spotify()
     ui = ConsoleUI()
-    music_util = MusicUtil(spotify_client_wrapper, ui.tell_user)
-    my_music_lib = MyMusicLib(spotify_client_wrapper, music_util, ui.tell_user)
-    MusicLibBot(spotify_client_wrapper, my_music_lib, music_util, ui).run()
+    music_util = MusicUtil(spotify, ui.tell_user)
+    my_music_lib = MyMusicLib(spotify, music_util, ui.tell_user)
+    MusicLibBot(spotify, my_music_lib, music_util, ui).run()
 
 
 if __name__ == "__main__":

src/packages/music_api_clients/models/album.py
@@ -1,31 +1,31 @@
-from app.models.track import Track
-from app.models.artist import Artist
+from packages.music_api_clients.models.track import Track
+from packages.music_api_clients.models.artist import Artist
 from datetime import datetime
 
 
 class Album:
-    def __init__(self, name, id_, tracks, artists, release_date, num_tracks, genres=None, popularity=None):
+    def __init__(self, name, tracks, artists, release_date, num_tracks, spotify_id=None, genres=None, popularity=None):
         """
         Params:
             name (str).
-            id (str).
             artists ([dict]).
             release_date (datetime).
             num_tracks (int).
+            spotify_id (str).
             genres ([str]), optional.
             popularity (int) in range [0, 100], optional.
         """
         self.name = name
-        self.id = id_
         self.tracks = tracks
         self.artists = artists
         self.release_date = release_date
         self.num_tracks = num_tracks
+        self.spotify_id = spotify_id
         self.genres = genres
         self.popularity = popularity
 
     def __key(self):
-        return self.id
+        return self.spotify_id
 
     def __hash__(self):
         return hash(self.__key())
@@ -41,10 +41,12 @@ class Album:
     def set_popularity(self, popularity):
         self.popularity = popularity
 
+    def contains_track(self, track):
+        return self.spotify_id == track.spotify_album_id
+
     def from_spotify_album(spotify_album):
         return Album(
             spotify_album['name'],
-            spotify_album['id'],
             [
                 Track.from_spotify_album_track(track, spotify_album['id'])
                 for track in spotify_album['tracks']['items']
@@ -55,7 +57,21 @@ class Album:
             ],
             Album._parse_date(spotify_album['release_date']),
             spotify_album['total_tracks'],
+            spotify_id=spotify_album['id'],
             popularity=spotify_album['popularity'],
+        )
+
+    def from_spotify_artist_album(spotify_album):
+        return Album(
+            spotify_album['name'],
+            None,
+            [
+                Artist.from_spotify_album_artist(artist)
+                for artist in spotify_album['artists']
+            ],
+            Album._parse_date(spotify_album['release_date']),
+            spotify_album['total_tracks'],
+            spotify_id=spotify_album['id'],
         )
 
     def _parse_date(spotify_release_date):

src/packages/music_api_clients/models/artist.py
@@ -1,23 +1,23 @@
 class Artist:
-    def __init__(self, name, id_, uri, popularity=None, genres=None, albums=None):
+    def __init__(self, name, spotify_id=None, spotify_uri=None, popularity=None, genres=None, albums=None):
         """
         Params:
             name (str).
-            id_ (str).
-            uri (str).
+            spotify_id (str).
+            spotify_uri (str).
             popularity (int|None) in range [0, 100], optional.
             genres ([str]|None), optional.
             albums ([Album]).
         """
         self.name = name
-        self.id = id_
-        self.uri = uri
+        self.spotify_id = spotify_id
+        self.spotify_uri = spotify_uri
         self.popularity = popularity
         self.genres = genres
         self.albums = albums
 
     def __key(self):
-        return self.id
+        return self.spotify_id
 
     def __hash__(self):
         return hash(self.__key())
@@ -36,8 +36,8 @@ class Artist:
     def from_spotify_artist(spotify_artist):
         return Artist(
             spotify_artist['name'],
-            spotify_artist['id'],
-            spotify_artist['uri'],
+            spotify_id=spotify_artist['id'],
+            spotify_uri=spotify_artist['uri'],
             popularity=spotify_artist['popularity'],
             genres=spotify_artist['genres'],
         )
@@ -45,20 +45,20 @@ class Artist:
     def from_spotify_track_artist(spotify_artist):
         return Artist(
             spotify_artist['name'],
-            spotify_artist['id'],
-            spotify_artist['uri'],
+            spotify_id=spotify_artist['id'],
+            spotify_uri=spotify_artist['uri'],
         )
 
     def from_spotify_album_track_artist(spotify_artist):
         return Artist(
             spotify_artist['name'],
-            spotify_artist['id'],
-            spotify_artist['uri'],
+            spotify_id=spotify_artist['id'],
+            spotify_uri=spotify_artist['uri'],
         )
 
     def from_spotify_album_artist(spotify_artist):
         return Artist(
             spotify_artist['name'],
-            spotify_artist['id'],
-            spotify_artist['uri'],
+            spotify_id=spotify_artist['id'],
+            spotify_uri=spotify_artist['uri'],
         )

src/packages/music_api_clients/models/playlist.py
@@ -1,16 +1,16 @@
-from app.models.track import Track
+from packages.music_api_clients.models.track import Track
 
 class Playlist:
-    def __init__(self, id_, name, description, tracks_fetcher):
-        self.id = id_
+    def __init__(self, name, description, tracks_fetcher, spotify_id=None):
         self.name = name
         self.description = description
         self.tracks_fetcher = tracks_fetcher
+        self.spotify_id = spotify_id
         self.tracks = None
         self.num_tracks = None
 
     def __key(self):
-        return self.id
+        return self.spotify_id
 
     def __hash__(self):
         return hash(self.__key())
@@ -32,22 +32,28 @@ class Playlist:
             self.num_tracks = len(self.tracks)
         return self.num_tracks
 
+    def has_any_tracks_from_album(self, album):
+        for track in self.get_tracks():
+            if album.contains_track(track):
+                return True
+        return False
+
     def from_spotify_playlist(spotify_playlist):
         return Playlist(
-            spotify_playlist['id'],
             spotify_playlist['name'],
             spotify_playlist['description'],
             lambda: [
                 Track.from_spotify_playlist_track(track)
                 for track in spotify_playlist['tracks']['items']
             ],
+            spotify_id=spotify_playlist['id'],
         )
 
     def from_spotify_playlist_search_results(spotify_playlist, tracks_fetcher):
         playlist = Playlist(
-            spotify_playlist['id'],
             spotify_playlist['name'],
             spotify_playlist['description'],
             tracks_fetcher,
+            spotify_id=spotify_playlist['id'],
         )
         return playlist

src/packages/music_api_clients/models/song_attribute_ranges.py
@@ -1,4 +1,4 @@
-from app.models.audio_features import AudioFeatures
+from packages.music_api_clients.models.audio_features import AudioFeatures
 
 
 class SongAttributeRanges:

src/packages/music_api_clients/models/track.py
@@ -1,24 +1,24 @@
-from app.models.artist import Artist
+from packages.music_api_clients.models.artist import Artist
 
 
 class Track:
-    def __init__(self, name, id_, uri, album_id, artists, disc_number, duration_ms, popularity, track_number, audio_features=None):
+    def __init__(self, name, artists, disc_number, duration_ms, popularity, track_number, spotify_album_id=None, spotify_id=None, spotify_uri=None, audio_features=None):
         self.name = name
-        self.id = id_
-        self.uri = uri
-        self.album_id = album_id
         self.artists = artists
         self.disc_number = disc_number
         self.duration_ms = duration_ms
         self.popularity = popularity
         self.track_number = track_number
+        self.spotify_album_id = spotify_album_id
+        self.spotify_id = spotify_id
+        self.spotify_uri = spotify_uri
         self.audio_features = audio_features
 
     def set_audio_features(self, audio_features):
         self.audio_features = audio_features
 
     def __key(self):
-        return self.id
+        return self.spotify_id
 
     def __hash__(self):
         return hash(self.__key())
@@ -27,6 +27,12 @@ class Track:
         if isinstance(other, Track):
             return self.__key() == other.__key()
         return NotImplemented
+
+    def on_spotify(self):
+        return self.spotify_id[:13] != "spotify:local"
+
+    def in_any_of_albums(self, albums):
+        return self.spotify_album_id in [album.spotify_id for album in albums]
 
     def from_spotify_playlist_track(spotify_playlist_track):
         """
@@ -42,9 +48,6 @@ class Track:
         """
         return Track(
             spotify_track['name'],
-            spotify_track['id'],
-            spotify_track['uri'],
-            spotify_track['album']['id'],
             [
                 Artist.from_spotify_track_artist(artist)
                 for artist in spotify_track['artists']
@@ -53,14 +56,14 @@ class Track:
             spotify_track['duration_ms'],
             spotify_track['popularity'],
             spotify_track['track_number'],
+            spotify_album_id=spotify_track['album']['id'],
+            spotify_id=spotify_track['id'],
+            spotify_uri=spotify_track['uri'],
         )
 
-    def from_spotify_album_track(spotify_track, album_id):
+    def from_spotify_album_track(spotify_track, spotify_album_id):
         return Track(
             spotify_track['name'],
-            spotify_track['id'],
-            spotify_track['uri'],
-            album_id,
             [
                 Artist.from_spotify_album_track_artist(artist)
                 for artist in spotify_track['artists']
@@ -69,4 +72,7 @@ class Track:
             spotify_track['duration_ms'],
             None,
             spotify_track['track_number'],
+            spotify_album_id=spotify_album_id,
+            spotify_id=spotify_track['id'],
+            spotify_uri=spotify_track['uri'],
         )

commit 7496e1bf06808c8ba11143c625ba031af7978f50

Fix bug introduced in #55

src/packages/music_management/playlist_creator.py
@@ -58,10 +58,16 @@ class PlaylistCreator:
         return albums
 
     def duplicate_and_reduce_num_tracks_per_album(self, get_playlist, get_new_playlist_name, get_num_tracks_per_album):
+        """
+        Params:
+            get_playlist (Func): no args, return Playlist.
+            get_new_playlist_name (Func): no args, return string.
+            get_num_tracks_per_album (Func): no args, return int.
+        """
         tracks_by_album = defaultdict(list)
         playlist = get_playlist()
         for track in playlist.get_tracks():
-            tracks_by_album[track].append(track)
+            tracks_by_album[track.spotify_album_id].append(track)
 
         most_popular_tracks_per_album, num_tracks_per_album = [], get_num_tracks_per_album()
         for _, tracks in tracks_by_album.items():